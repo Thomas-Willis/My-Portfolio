7299980ee26c51a9f33d59265011495aaa518a45
Thomas-Willis <137636295+Thomas-Willis@users.noreply.github.com>
2023-07-11 01:16:45 +0100

    Modification of portfolio & about me sections.
    Addition of a project to the portfolio section.
    Modification of the SCSS to display this addition correctly.
    Adding responsiveness to the about me section.

---
+++ script/buttons.js
@@ -0,0 +1,5 @@
+const sushiButton = document.getElementById("sushi-button")
+
+sushiButton.addEventListener("click", function(){
+    window.open("https://github.com/Thomas-Willis/Sushi", "_blank");
+});
